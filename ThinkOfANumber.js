502689c743a76375dbece9ebfc2c0fc5b8c51d6c
mollypettit <molly_pettit@outlook.com>
2018-03-10 02:44:46 +0000

    updated pages

--- ThinkOfANumber.js
+++ ThinkOfANumber.js
@@ -1,1 +1,24 @@
-//fix thiss!!
+//variables
+let btnGuess = document.getElementById("btnGuess");
+let output = document.getElementById("output");
+let guess = document.getElementById("guess");
+let number = Math.floor(Math.random() * 100 + 1);
+console.log(number);
+let guesses = 0;
+
+btnGuess.addEventListener('click', guessNumber);
+
+//functions
+
+ function guessNumber() {
+    guesses++;
+    let input = document.getElementById("guess").value;
+    console.log(input);
+     
+    if (guess < number) {
+        output.innerHTML = "Your guess is too low, try again!";
+    }
+    else if (guess > number) {
+        output.innerHTML = "Your guess is too high, try again!";
+    }
+}
